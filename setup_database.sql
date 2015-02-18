afb9832d7fb71f39b63c535096b0047b7ee996d0
Niels H <niels.hofmans@dhl.com>
2015-02-18 23:54:22 +0100

    Added MySQL code + default lang.

--- setup_database.sql
+++ setup_database.sql
@@ -1,7 +1,47 @@
+#MySQL
+CREATE SCHEMA gunther;
+
+USE gunther;
+
 CREATE TABLE users (
   id SERIAL NOT NULL,
   login VARCHAR(10) NOT NULL,
-  password VARCHAR(50) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  email TEXT,
+  lastseen TIMESTAMP,
+  role INT NOT NULL,
+  PRIMARY KEY  (id)
+);
+
+CREATE TABLE langs (
+  id SERIAL NOT NULL,
+  name VARCHAR(15) NOT NULL,
+  flag VARCHAR(2) NOT NULL,
+  PRIMARY KEY  (id)
+);
+
+CREATE TABLE roles (
+  id SERIAL NOT NULL,
+  name VARCHAR(20) NOT NULL,
+  PRIMARY KEY  (id)
+);
+
+
+ALTER TABLE users ADD CONSTRAINT users_fk1 FOREIGN KEY (role) REFERENCES roles(id);
+
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO users (login, password, role) VALUES ('admin', '52f504477e36154a49464ed65a50dd3b97c99327', 1);
+INSERT INTO langs (name, flag) VALUES ('English', 'en');
+
+
+#PosgreSQL
+CREATE SCHEMA gunther;
+
+CREATE TABLE users (
+  id SERIAL NOT NULL,
+  login VARCHAR(10) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT(NOW()),
   email TEXT,
   lastseen TIMESTAMP,
@@ -27,3 +67,4 @@ ALTER TABLE users ADD CONSTRAINT users_fk1 FOREIGN KEY (role) REFERENCES roles(i
 
 INSERT INTO roles (name) VALUES ('admin');
 INSERT INTO users (login, password, role) VALUES ('admin', '52f504477e36154a49464ed65a50dd3b97c99327', 1);
+INSERT INTO langs (name, flag) VALUES ('English', 'en');
