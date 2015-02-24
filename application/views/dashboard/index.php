57596d81ef114c543c84ed8ed4f714cbeee401ab
Niels H <niels.hofmans@dhl.com>
2015-02-24 12:28:27 +0100

    Added checks in view.

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -8,17 +8,20 @@
 
         <h3><?= $this->lang['recentmovies']; ?></h3><hr>
         <div class="slider-movies">
+        <? if ($this->movies): ?>
             <? foreach ($this->movies as $movie): ?>
             <a href="/watch/index/<?= $movie->info->imdb; ?>">
 	           <img class="owl-lazy" alt="<?= $movie->info->original_title; ?>" data-src="<?= $movie->info->images->poster[0]; ?>" />
             </a>
             <? endforeach; ?>
+        <? endif; ?>
         </div>
 
         <br>
         
         <h3><?= $this->lang['recentepi']; ?></h3><hr>
         <div class="slider-series">
+        <? if ($this->episodes): ?>
             <? foreach ($this->episodes as $episode): ?>
             <a href="/watch/index/ss<?= $episode->serieId . '-' . $episode->season . '-' . $episode->number; ?>">
             <div class="fix">
@@ -29,5 +32,6 @@
             </div>
             </a>
             <? endforeach; ?>
+        <? endif; ?>
         </div>
 </div>
