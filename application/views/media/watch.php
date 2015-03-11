526aac2de723c6c645d534a84072352588a4e462
HazCod <HazCod@users.noreply.github.com>
2015-03-11 12:42:37 +0100

    Update watch.php
    Added tech order to fallback to flash

--- application/views/media/watch.php
+++ application/views/media/watch.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-md-12">
-		<video id="video" class="video-js vjs-default-skin vjs-fullscreen video-js-fullscreen vjs-big-play-centered" controls preload="none" data-setup="{}" width="auto" height="auto">
+		<video id="video" class="video-js vjs-default-skin vjs-fullscreen video-js-fullscreen vjs-big-play-centered" controls preload="none" data-setup='{"techOrder": ["html5", "flash"]}' width="auto" height="auto">
 		    <source src="/watch/stream/<?= $this->streamstr; ?>" type='video/mp4' /> <!-- type='<?= $this->type; ?>' codecs="<?= $this->codec; ?>" /> -->
 
 			<? foreach ($this->subs as $sub): ?>
