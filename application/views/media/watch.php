4fd07b649d14061b46dedadff99f2d5a43de6590
HazCod <HazCod@users.noreply.github.com>
2015-03-11 19:39:11 +0100

    Update watch.php
    Changed TechOrder.

--- application/views/media/watch.php
+++ application/views/media/watch.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-md-12">
-		<video id="video" class="video-js vjs-default-skin vjs-fullscreen video-js-fullscreen vjs-big-play-centered" controls preload="none" data-setup='{"techOrder": ["vlc", "html5", "flash"]}' width="auto" height="auto">
-		    <source src="/watch/stream/<?= $this->streamstr; ?>" type='video/mp4' /> <!-- type='<?= $this->type; ?>' codecs="<?= $this->codec; ?>" /> -->
+		<video id="video" class="video-js vjs-default-skin vjs-fullscreen video-js-fullscreen vjs-big-play-centered" controls preload="none" data-setup='{"techOrder": ["html5", "flash", "vlc"]}' width="auto" height="auto">
+		    <source src="/watch/stream/<?= $this->streamstr; ?>" type='video/mp4' codecs="<?= $this->codec; ?>" /> <!-- type='<?= $this->type; ?>' codecs="<?= $this->codec; ?>" /> -->
 
 			<? foreach ($this->subs as $sub): ?>
 				<track src="/watch/sub/<?= $this->file; ?>/<?= $sub; ?>" srclang="<?= $sub; ?>" label="<?= $sub; ?>"/>
