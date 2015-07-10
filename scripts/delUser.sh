447fbe66fc36e71aef8a7dfa88c38dfa154a106a
root <root@gunther.colo.transip.net>
2015-07-10 13:25:31 +0200

    Fix broken delUser script.

--- scripts/delUser.sh
+++ scripts/delUser.sh
@@ -8,10 +8,7 @@ AUTH_REALM=Media
 
 if [ "$#" -eq 2 ]; then
 	if [ -f "$HTDIGEST_FILE" ]; then
-		# remove line number x (if file exists)
-		sed -e "$2 d" "$HTDIGEST_FILE" > /var/www/digest_temp
-		cat /var/www/digest_temp > "$HTDIGEST_FILE"
-		rm /var/www/digest_temp
+		sed -i "/$username:Media/d" "$HTDIGEST_FILE"
 	fi
 else
 	echo "ERROR: Expects two arguments. (digest_file and username)"
