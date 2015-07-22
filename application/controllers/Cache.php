3cb966141b513c496d06acb1392b7478dae1308b
HazCod <HazCod@users.noreply.github.com>
2015-07-22 16:28:37 +0200

    Update Cache.php
    Only allow localhost to clear cache. Could allow remote users to bring down server/cp/sr.
    UPDATE IMMEDIATELY

--- application/controllers/Cache.php
+++ application/controllers/Cache.php
@@ -5,6 +5,18 @@ class Cache extends Core_controller
     public function __construct()
     {
         parent::__construct(false);
+        
+        $whitelist = array(
+            '127.0.0.1',
+            '::1',
+            'localhost'
+        );
+
+        if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+            // only allow localhost
+            header("HTTP/1.1 401 Unauthorized");
+            die();
+        }
     }
 
     public function index()
