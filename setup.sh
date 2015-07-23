73c54e1d10168dc734202bb8616b17af7e96b49c
HazCod <HazCod@users.noreply.github.com>
2015-07-23 13:46:34 +0200

    Update setup.sh
    Added more security constraints

--- setup.sh
+++ setup.sh
@@ -88,8 +88,7 @@ openssl req \
     -sha256
     
 #create DHE parameters, instead of 1024 default ones
-cd /etc/ssl/certs
-#openssl dhparam -out dhparam.pem 4096
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
 #create nginx config
 cat > /etc/nginx/conf/nginx.conf << EOF
@@ -114,6 +113,12 @@ http {
     upstream php {
         server unix:/tmp/php5-fpm/sock;
     }
+        #Don't send version of nginx
+        server_tokens off;
+        
+	add_header X-Frame-Options SAMEORIGIN;
+	add_header X-Content-Type-Options nosniff;
+	add_header X-XSS-Protection "1; mode=block";
         
 	#Only use secure ciphers
 	ssl_ciphers 'EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH';
@@ -174,8 +179,22 @@ http {
                 rewrite     ^   https://\$server_name\$request_uri? permanent;
         }
         server {
-                listen 443 ssl;
+                listen 443 ssl default deferred;
                 
+		ssl_session_cache shared:SSL:50m;
+		ssl_session_timeout 5m;
+		
+		ssl_dhparam /etc/ssl/certs/dhparam.pem;
+		
+		ssl_prefer_server_ciphers on;
+		ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+		ssl_ciphers "ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES128-SHA256:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256:AES256-SHA:AES128-SHA:DES-CBC3-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4";
+
+		resolver 8.8.8.8;
+		ssl_stapling on;
+		
+		add_header Strict-Transport-Security "max-age=31536000; includeSubdomains;";
+
                 # Only allow over HTTPS
                 add_header Strict-Transport-Security "max-age=63072000; includeSubdomains; preload";
                 # Use stronger DHE parameter
